7aa4a51d623110a4f67fe5a80430575d2a9d49d5
andygiannini <andy@onedesigncompany.com>
2016-01-23 15:05:22 -0600

    Musical power!

--- party.js
+++ party.js
@@ -1,20 +1,18 @@
 function PartyTime() {
-  var _this = this;
-
   var _swapImages = function() {
     var images = document.getElementsByTagName('img');
     for (var index = 0; index < images.length; index++) {
-      images[index].src = 'http://www.nerdgranny.com/wp-content/uploads/2008/06/ascii-goatse.gif';
+      images[index].src = 'http://i.imgur.com/DLXvBgs.gif';
     }
   }
 
   var _imageParty = function() {
     _swapImages();
-    //DOMNodeInserted
+    document.addEventListener('DOMNodeInserted', _swapImages);
   }
 
   var _marqueeParty = function() {
-    var tags = ['span', 'p', 'li', 'h1', 'h2', 'h3', 'h4', 'h5', 'a', 'td', 'pre', 'label'];
+    var tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'a', 'td', 'pre', 'label'];
     for(var y = 0; y < tags.length; y++) {
       var party = document.getElementsByTagName(tags[y]);
       for(var x = 0; x < party.length; x++) {
@@ -24,12 +22,20 @@ function PartyTime() {
     }
   }
 
+  var _mouseParty = function() {
+  }
+
+  var _soundParty = function() {
+    var music = document.createElement('div');
+    music.innerHTML = '<iframe width="1" height="1" src="https://www.youtube.com/embed/ujKRFbPYUmM?autoplay=1" frameborder="0"></iframe>';
+    document.body.appendChild(music);
+  }
+
   var _init = function() {
     _imageParty();
     _marqueeParty();
-    //_mouseParty();
-    //_soundParty();
-    alert('GET READY TO PARTY');
+    _mouseParty();
+    _soundParty();
   }
 
   _init();
